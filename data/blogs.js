91a7915ffbca25ec85691c88679e20b9f68cd758
Mohd Salim <smsalim931@gmail.com>
2025-05-20 07:31:16 +0530

    first commit

---
+++ data/blogs.js
@@ -0,0 +1,28 @@
+blogs = [
+        {
+            title: 'My First Blog Post',
+            content: 'This is the content of my first blog post.',
+        },
+        {
+            title: 'My Second Blog Post',
+            content: 'This is the content of my second blog post.',
+        },
+        {
+            title: 'My Third Blog Post',
+            content: 'This is the content of my third blog post.',
+        },
+        {
+            title: 'My Fourth Blog Post',
+            content: 'This is the content of my fourth blog post.',
+        },
+        {
+            title: 'My Fifth Blog Post',
+            content: 'This is the content of my fifth blog post.',
+        },
+        {
+            title: 'My Sixth Blog Post',
+            content: 'This is the content of my sixth blog post.',
+        },
+    ]
+
+module.exports = blogs
--- routers/blog.js
+++ routers/blog.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
+const blogs = require('../data/blogs.js')
 const router = express.Router()
+
 
 router.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../templates/index.html'))
 })
 
+router.get('/blog', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../templates/blog.html'))
+})
 
 module.exports = router
